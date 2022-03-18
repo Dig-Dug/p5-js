435afbd8a318e19453714b23f7634138e3949afa
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-03-18 11:33:11 +0100

    -

--- math.js
+++ math.js
@@ -164,3 +164,107 @@ ellipse(i,j, size, size);
 }
 
 //Sine---------------------------------------------
+let diameter;
+let angle = 0;
+
+function setup() {
+  createCanvas(710, 400);
+  diameter = height - 10;
+  noStroke();
+  fill(255, 204, 0);
+}
+
+function draw() {
+  background(0);
+
+  let d1 = 10 + (sin(angle) * diameter) / 2 + diameter / 2;
+  let d2 = 10 + (sin(angle + PI / 2) * diameter) / 2 + diameter / 2;
+  let d3 = 10 + (sin(angle + PI) * diameter) / 2 + diameter / 2;
+
+  ellipse(0, height / 2, d1, d1);
+  ellipse(width / 2, height / 2, d2, d2);
+  ellipse(width, height / 2, d3, d3);
+
+  angle += 0.02;
+}
+//Sine Cosine----------------------------------------
+let angle1 = 0;
+let angle2 = 0;
+let scalar = 70;
+
+function setup() {
+  createCanvas(710, 400);
+  noStroke();
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(0);
+
+  let ang1 = radians(angle1);
+  let ang2 = radians(angle2);
+
+  let x1 = width / 2 + scalar * cos(ang1);
+  let x2 = width / 2 + scalar * cos(ang2);
+
+  let y1 = height / 2 + scalar * sin(ang1);
+  let y2 = height / 2 + scalar * sin(ang2);
+
+  fill(255);
+  rect(width * 0.5, height * 0.5, 140, 140);
+
+  fill(0, 102, 153);
+  ellipse(x1, height * 0.5 - 120, scalar, scalar);
+  ellipse(x2, height * 0.5 + 120, scalar, scalar);
+
+  fill(255, 204, 0);
+  ellipse(width * 0.5 - 120, y1, scalar, scalar);
+  ellipse(width * 0.5 + 120, y2, scalar, scalar);
+
+  angle1 += 2;
+  angle2 += 3;
+}
+//Sine wave------------------------------------
+
+let xspacing = 16; // Distance between each horizontal location
+let w; // Width of entire wave
+let theta = 0.0; // Start angle at 0
+let amplitude = 75.0; // Height of wave
+let period = 500.0; // How many pixels before the wave repeats
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+
+function setup() {
+  createCanvas(710, 400);
+  w = width + 16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w / xspacing));
+}
+
+function draw() {
+  background(0);
+  calcWave();
+  renderWave();
+}
+
+function calcWave() {
+  // Increment theta (try different values for
+  // 'angular velocity' here)
+  theta += 0.02;
+
+  // For every x value, calculate a y value with sine function
+  let x = theta;
+  for (let i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x) * amplitude;
+    x += dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  fill(255);
+  // A simple way to draw the wave with an ellipse at each location
+  for (let x = 0; x < yvalues.length; x++) {
+    ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
+  }
+}
