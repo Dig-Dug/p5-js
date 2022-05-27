9cad7e3e6fd26dca031e62523c17dbde75f76559
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-05-27 11:51:33 +0200

    -

--- simulate0.js
+++ simulate0.js
@@ -166,3 +166,233 @@ function drawIt(k) {
 }
 
 /////Spring-------------------------------------------
+// Spring drawing constants for top bar
+let springHeight = 32,
+    left,
+    right,
+    maxHeight = 200,
+    minHeight = 100,
+    over = false,
+    move = false;
+
+// Spring simulation constants
+let M = 0.8,  // Mass
+    K = 0.2,  // Spring constant
+    D = 0.92, // Damping
+    R = 150;  // Rest position
+
+// Spring simulation variables
+let ps = R,   // Position
+    vs = 0.0, // Velocity
+    as = 0,   // Acceleration
+    f = 0;    // Force
+
+function setup() {
+  createCanvas(710, 400);
+  rectMode(CORNERS);
+  noStroke();
+  left = width / 2 - 100;
+  right = width / 2 + 100;
+}
+
+function draw() {
+  background(102);
+  updateSpring();
+  drawSpring();
+}
+
+function drawSpring() {
+  // Draw base
+  fill(0.2);
+  let baseWidth = 0.5 * ps + -8;
+  rect(width / 2 - baseWidth, ps + springHeight, width / 2 + baseWidth, height);
+
+  // Set color and draw top bar
+  if (over || move) {
+    fill(255);
+  } else {
+    fill(204);
+  }
+
+  rect(left, ps, right, ps + springHeight);
+}
+
+function updateSpring() {
+  // Update the spring position
+  if ( !move ) {
+    f = -K * ( ps - R ); // f=-ky
+    as = f / M;          // Set the acceleration, f=ma == a=f/m
+    vs = D * (vs + as);  // Set the velocity
+    ps = ps + vs;        // Updated position
+  }
+
+  if (abs(vs) < 0.1) {
+    vs = 0.0;
+  }
+
+  // Test if mouse if over the top bar
+  if (mouseX > left && mouseX < right && mouseY > ps && mouseY < ps + springHeight) {
+    over = true;
+  } else {
+    over = false;
+  }
+
+  // Set and constrain the position of top bar
+  if (move) {
+    ps = mouseY - springHeight / 2;
+    ps = constrain(ps, minHeight, maxHeight);
+  }
+}
+
+function mousePressed() {
+  if (over) {
+    move = true;
+  }
+}
+
+function mouseReleased() {
+  move = false;
+}
+//Springs-----------------------------------------------------------
+let num = 3;
+let springs = [];
+
+function setup() {
+  createCanvas(710, 400);
+  noStroke();
+
+  springs[0] = new Spring(240, 260, 40, 0.98, 8.0, 0.1, springs, 0);
+  springs[1] = new Spring(320, 210, 120, 0.95, 9.0, 0.1, springs, 1);
+  springs[2] = new Spring(180, 170, 200, 0.90, 9.9, 0.1, springs, 2);
+}
+
+function draw() {
+  background(51);
+
+  for (let i = 0; i < num; i++) {
+	springs[i].update();
+	springs[i].display();
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < num; i++) {
+    springs[i].pressed();
+  }
+}
+
+function mouseReleased() {
+  for (let i = 0; i < num; i++) {
+	springs[i].released();
+  }
+}
+
+// Spring class
+function Spring (_x, _y, _s, _d, _m, _k_in, _others, _id) {
+  // Screen values
+  // this.xpos = _x;
+  // this.ypos = _y;
+
+  this.x_pos = _x;
+  this.y_pos= _y;
+
+  this.size = 20;
+  this.size = _s;
+
+  this.over = false;
+  this.move = false;
+
+	// Spring simulation constants
+  this.mass = _m;       // Mass
+  this.k = 0.2;    // Spring constant
+  this.k = _k_in;
+  this.damp = _d;       // Damping
+  this.rest_posx = _x;  // Rest position X
+  this.rest_posy = _y;  // Rest position Y
+
+  // Spring simulation variables
+  //float pos = 20.0; // Position
+  this.velx = 0.0;   // X Velocity
+  this.vely = 0.0;   // Y Velocity
+  this.accel = 0;    // Acceleration
+  this.force = 0;    // Force
+
+  this.friends = _others;
+  this.id = _id;
+
+  this.update = function() {
+
+	if (this.move) {
+	  this.rest_posy = mouseY;
+	  this.rest_posx = mouseX;
+	}
+
+	this.force = -this.k * (this.y_pos - this.rest_posy);  // f=-ky
+	this.accel = this.force / this.mass;                 // Set the acceleration, f=ma == a=f/m
+	this.vely = this.damp * (this.vely + this.accel);         // Set the velocity
+	this.y_pos = this.y_pos + this.vely;           // Updated position
+
+
+	this.force = -this.k * (this.x_pos - this.rest_posx);  // f=-ky
+	this.accel = this.force / this.mass;                 // Set the acceleration, f=ma == a=f/m
+	this.velx = this.damp * (this.velx + this.accel);         // Set the velocity
+	this.x_pos = this.x_pos + this.velx;           // Updated position
+
+
+	if ((this.overEvent() || this.move) && !(this.otherOver()) ) {
+	  this.over = true;
+	} else {
+	    this.over = false;
+	  }
+  }
+
+  // Test to see if mouse is over this spring
+  this.overEvent = function() {
+	let disX = this.x_pos - mouseX;
+	let disY = this.y_pos - mouseY;
+	let dis = createVector(disX, disY);
+	if (dis.mag() < this.size / 2 ) {
+	  return true;
+	} else {
+		return false;
+	  }
+  }
+
+  // Make sure no other springs are active
+  this.otherOver = function() {
+	for (let i = 0; i < num; i++) {
+	  if (i != this.id) {
+		if (this.friends[i].over == true) {
+		  return true;
+		}
+	  }
+	}
+	return false;
+  }
+
+  this.display = function() {
+	if (this.over) {
+	  fill(153);
+	} else {
+	    fill(255);
+	  }
+	ellipse(this.x_pos, this.y_pos, this.size, this.size);
+  }
+
+  this.pressed = function() {
+	if (this.over) {
+	  this.move = true;
+	} else {
+	  this.move = false;
+	}
+  }
+
+  this.released = function() {
+	this.move = false;
+	this.rest_posx = this.y_pos;
+	this.rest_posy = this.y_pos;
+  }
+};
+
+
+//SOFT BODY::::::::::::::::::::::::::::::::::::::::::::::::
