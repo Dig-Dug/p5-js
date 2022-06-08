3481f2efe0fb027de0b0df5945d9d0d4848890e2
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-06-08 11:20:19 +0200

    -

--- simulate0.js
+++ simulate0.js
@@ -396,3 +396,329 @@ function Spring (_x, _y, _s, _d, _m, _k_in, _others, _id) {
 
 
 //SOFT BODY::::::::::::::::::::::::::::::::::::::::::::::::
+// center point
+let centerX = 0.0, centerY = 0.0;
+
+let radius = 45, rotAngle = -90;
+let accelX = 0.0, accelY = 0.0;
+let deltaX = 0.0, deltaY = 0.0;
+let springing = 0.0009, damping = 0.98;
+
+//corner nodes
+let nodes = 5;
+
+//zero fill arrays
+let nodeStartX = [];
+let nodeStartY = [];
+let nodeX = [];
+let nodeY = [];
+let anglee = [];
+let frequency = [];
+
+// soft-body dynamics
+let organicConstant = 1.0;
+
+function setup() {
+  createCanvas(710, 400);
+
+  //center shape in window
+  centerX = width / 2;
+  centerY = height / 2;
+
+  //initialize arrays to 0
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = 0;
+    nodeStartY[i] = 0;
+    nodeY[i] = 0;
+    nodeY[i] = 0;
+    angle[i] = 0;
+  }
+
+  // iniitalize frequencies for corner nodes
+  for (let i = 0; i < nodes; i++){
+    frequency[i] = random(5, 12);
+  }
+
+  noStroke();
+  frameRate(30);
+}
+
+function draw() {
+  //fade background
+  fill(0, 100);
+  rect(0, 0, width, height);
+  drawShape();
+  moveShape();
+}
+
+function drawShape() {
+  //  calculate node  starting locations
+  for (let i = 0; i < nodes; i++){
+    nodeStartX[i] = centerX + cos(radians(rotAngle)) * radius;
+    nodeStartY[i] = centerY + sin(radians(rotAngle)) * radius;
+    rotAngle += 360.0 / nodes;
+  }
+
+  // draw polygon
+  curveTightness(organicConstant);
+  fill(255);
+  beginShape();
+  for (let i = 0; i < nodes; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  for (let i = 0; i < nodes-1; i++){
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  endShape(CLOSE);
+}
+
+function moveShape() {
+  //move center point
+  deltaX = mouseX - centerX;
+  deltaY = mouseY - centerY;
+
+  // create springing effect
+  deltaX *= springing;
+  deltaY *= springing;
+  accelX += deltaX;
+  accelY += deltaY;
+
+  // move predator's center
+  centerX += accelX;
+  centerY += accelY;
+
+  // slow down springing
+  accelX *= damping;
+  accelY *= damping;
+
+  // change curve tightness
+  organicConstant = 1 - ((abs(accelX) + abs(accelY)) * 0.1);
+
+  //move nodes
+  for (let i = 0; i < nodes; i++){
+    nodeX[i] = nodeStartX[i] + sin(radians(angle[i])) * (accelX * 2);
+    nodeY[i] = nodeStartY[i] + sin(radians(angle[i])) * (accelY * 2);
+    angle[i] += frequency[i];
+  }
+}
+
+
+//smoke Particles----------------------------------------------------
+
+// texture for the particle
+let particle_texture = null;
+
+// variable holding our particle system
+let ps = null;
+
+function preload() {
+  particle_texture = loadImage("assets/particle_texture.png");
+}
+
+function setup() {
+
+  //set the canvas size
+  createCanvas(640, 360);
+
+  //initialize our particle system
+  ps = new ParticleSystem(0, createVector(width / 2, height - 60), particle_texture);
+}
+
+function draw() {
+  background(0);
+
+  let dx = map(mouseX, 0, width, -0.2, 0.2);
+  let wind = createVector(dx, 0);
+
+  ps.applyForce(wind);
+  ps.run();
+  for (let i = 0; i < 2; i++) {
+    ps.addParticle();
+  }
+
+  // Draw an arrow representing the wind force
+  drawVector(wind, createVector(width / 2, 50, 0), 500);
+}
+
+/**
+ *  This function draws an arrow showing the direction our "wind" is blowing.
+ */
+function drawVector(v, loc, scale){
+  push();
+  let arrowsize = 4;
+  translate(loc.x, loc.y);
+  stroke(255);
+  rotate(v.heading());
+
+  let len = v.mag() * scale;
+  line(0, 0, len,0);
+  line(len, 0, len-arrowsize, +arrowsize / 2);
+  line(len, 0, len-arrowsize, -arrowsize / 2);
+  pop();
+}
+//========= PARTICLE SYSTEM ===========
+
+/**
+ * A basic particle system class
+ * @param num the number of particles
+ * @param v the origin of the particle system
+ * @param img_ a texture for each particle in the system
+ * @constructor
+ */
+let ParticleSystem = function(num, v, img_) {
+
+  this.particles = [];
+  this.origin = v.copy(); // we make sure to copy the vector value in case we accidentally mutate the original by accident
+  this.img = img_
+  for(let i = 0; i < num; ++i){
+    this.particles.push(new Particle(this.origin, this.img));
+  }
+};
+
+/**
+ * This function runs the entire particle system.
+ */
+ParticleSystem.prototype.run = function() {
+
+  // cache length of the array we're going to loop into a variable
+  // You may see <variable>.length in a for loop, from time to time but
+  // we cache it here because otherwise the length is re-calculated for each iteration of a loop
+  let len = this.particles.length;
+
+  //loop through and run particles
+  for (let i = len - 1; i >= 0; i--) {
+    let particle = this.particles[i];
+    particle.run();
+
+    // if the particle is dead, we remove it.
+    // javascript arrays don't have a "remove" function but "splice" works just as well.
+    // we feed it an index to start at, then how many numbers from that point to remove.
+    if (particle.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+}
+
+/**
+ * Method to add a force vector to all particles currently in the system
+ * @param dir a p5.Vector describing the direction of the force.
+ */
+ParticleSystem.prototype.applyForce = function(dir) {
+  let len = this.particles.length;
+  for(let i = 0; i < len; ++i){
+    this.particles[i].applyForce(dir);
+  }
+}
+
+/**
+ * Adds a new particle to the system at the origin of the system and with
+ * the originally set texture.
+ */
+ParticleSystem.prototype.addParticle = function() {
+    this.particles.push(new Particle(this.origin, this.img));
+}
+
+//========= PARTICLE  ===========
+/**
+ *  A simple Particle class, renders the particle as an image
+ */
+let Particle = function (pos, img_) {
+  this.loc = pos.copy();
+
+  let vx = randomGaussian() * 0.3;
+  let vy = randomGaussian() * 0.3 - 1.0;
+
+  this.vel = createVector(vx, vy);
+  this.acc = createVector();
+  this.lifespan = 100.0;
+  this.texture = img_;
+}
+
+/**
+ *  Simulataneously updates and displays a particle.
+ */
+Particle.prototype.run = function() {
+  this.update();
+  this.render();
+}
+
+/**
+ *  A function to display a particle
+ */
+Particle.prototype.render = function() {
+  imageMode(CENTER);
+  tint(255, this.lifespan);
+  image(this.texture, this.loc.x, this.loc.y);
+}
+
+/**
+ *  A method to apply a force vector to a particle.
+ */
+Particle.prototype.applyForce = function(f) {
+  this.acc.add(f);
+}
+
+/**
+ *  This method checks to see if the particle has reached the end of it's lifespan,
+ *  if it has, return true, otherwise return false.
+ */
+Particle.prototype.isDead = function () {
+  if (this.lifespan <= 0.0) {
+    return true;
+  } else {
+      return false;
+    }
+}
+
+/**
+ *  This method updates the position of the particle.
+ */
+Particle.prototype.update = function() {
+  this.vel.add(this.acc);
+  this.loc.add(this.vel);
+  this.lifespan -= 2.5;
+  this.acc.mult(0);
+}
+
+//Brownian Motion-------------------
+
+let num = 2000;
+let range = 6;
+
+let ax = [];
+let ay = [];
+
+
+function setup() {
+  createCanvas(710, 400);
+  for ( let i = 0; i < num; i++ ) {
+    ax[i] = width / 2;
+    ay[i] = height / 2;
+  }
+  frameRate(30);
+}
+
+function draw() {
+  background(51);
+
+  // Shift all elements 1 place to the left
+  for ( let i = 1; i < num; i++ ) {
+    ax[i - 1] = ax[i];
+    ay[i - 1] = ay[i];
+  }
+
+  // Put a new value at the end of the array
+  ax[num - 1] += random(-range, range);
+  ay[num - 1] += random(-range, range);
+
+  // Constrain all points to the screen
+  ax[num - 1] = constrain(ax[num - 1], 0, width);
+  ay[num - 1] = constrain(ay[num - 1], 0, height);
+
+  // Draw a line connecting the points
+  for ( let j = 1; j < num; j++ ) {
+    let val = j / num * 204.0 + 51;
+    stroke(val);
+    line(ax[j - 1], ay[j - 1], ax[j], ay[j]);
+  }
+}
